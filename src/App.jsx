ba5a9ce0a2d33afc64e60c1c8e24333ef1856817
toriecr <torie.cr@gmail.com>
2026-05-31 10:52:33 +1000

    feat: Add inline css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import HousingChart from './components/HousingChart'
 
 function App() {
@@ -6,11 +5,15 @@ function App() {
 		<>
 			<header>
 				<p>Melbourne Housing Viz - ABS Census</p>
-				<h1>Buying in Melbourne: how many years of full-time pay for a median house?</h1>
-				<p>I built this to understand why buying in Melbourne feels out of reach. The median established house price has risen far faster than typical full-time wages. This chart shows that gap as “years of gross earnings” at the median. <i>It’s not a mortgage calculator; it doesn’t include deposits, interest rates, or the fact that many buyers are dual-income households.</i></p>
-				<p>Built with React and D3.js - Torie CR</p>
+				<h1 style={{ lineHeight: '90%' }}>Buying in Melbourne: how many years of full-time pay for a median house?</h1>
+				<p style={{ textAlign: 'left', padding: '1%' }}>I built this to understand why buying in Melbourne feels out of reach. The median established house price has risen far faster than typical full-time wages. This chart shows that gap as “years of gross earnings” at the median. <i>It’s not a mortgage calculator; it doesn’t include deposits, interest rates, or the fact that many buyers are dual-income households.</i></p>
 			</header>
-			<HousingChart />
+			<main style={{ flex: 1, }}>
+				<HousingChart />
+			</main>
+			<footer style={{ paddingBottom: '1%', fontSize: '14px' }}>
+				<p>Built with React and D3.js - Torie CR</p>
+			</footer>
 		</>
 	)
 }
